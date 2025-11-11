c9c017da181a15ffc9ccb66816ee017eb165a57a
flowerboycutie <markmarvindizon@gmail.com>
2025-11-11 19:59:26 +0800

    initial level design and game graphics

--- code/player.py
+++ code/player.py
@@ -5,8 +5,8 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((TILESIZE,TILESIZE))
-        self.image.fill((255, 0, 0))
-        #self.image = pygame.image.load('..rpg/graphics/test/player.png').convert_alpha()
+        #self.image.fill((255, 0, 0))
+        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-26)
 
